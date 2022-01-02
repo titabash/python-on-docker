fe0de01d31faac05785dae8995a0ac3203d98def
titabash <titabash0819@gmail.com>
2022-01-02 21:24:43 +0900

    テストディレクトリ削除

--- tools/init.sh
+++ tools/init.sh
@@ -1,7 +1,11 @@
 if [ -e ./src ]; then
   rm -rf ./src
 fi
+if [ -e ./tests ]; then
+  rm -rf ./tests
+fi
 mkdir ./src
-git clone https://github.com/titabash/python-clean-architecture-template.git ./src
+git clone --filter=blob:none --sparse https://github.com/titabash/python-clean-architecture-template.git ./
+git sparse-checkout set src
+git sparse-checkout set tests
 rm -rf ./src/.git
-rm -rf ./src/.gitignore
