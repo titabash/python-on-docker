bacfc202215cba2af84abc22fbdaf147af9ff071
titabash <titabash0819@gmail.com>
2022-01-04 12:37:22 +0900

    init時に確認処理を追加

--- tools/init.sh
+++ tools/init.sh
@@ -1,6 +1,13 @@
 CURRENT_DIR = $(pwd)
 if [ -e ./project ]; then
-  rm -rf ./project
+  read -n1 -p "Are you sure you want to permanently delete the project? (y/N): " yn
+  if [[ $yn = [yY] ]]; then
+    rm -rf ./project
+    echo "Project has been deleted."
+  else
+    echo abort
+    exit 0
+  fi
 fi
 
 git clone https://github.com/titabash/python-clean-architecture-template.git ./project
